e8ca7b5c07e0b5e90f0dd78f243e294c5344f00e
PietrzakAdrian <contact.pietrzakadrian@gmail.com>
2019-10-27 17:06:30 +0100

    install engine

---
+++ app/containers/App/index.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * App.js
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
+ */
+
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { hot } from 'react-hot-loader/root';
+
+import HomePage from '../HomePage/Loadable';
+
+import GlobalStyle from '../../global-styles';
+
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
+      <GlobalStyle />
+    </div>
+  );
+};
+
+export default hot(App);
